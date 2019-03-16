3f4296d66ba9df77f6441e4a7a2c06a2a6110329
sergiodomin <sergiodurjc@gmail.com>
2019-03-17 00:47:47 +0100

    Modify style and variables names

--- src/detection/preprocess.py
+++ src/detection/preprocess.py
@@ -6,88 +6,95 @@ class Preprocess():
     """
     Preprocesa la imagen para poder aplicar algoritmos sobre ella.
     Se recortan los bordes y se devuelve la imagen recortada
-    y una máscara de ella con los bordes dilatados.
+    y una mascara de ella con los bordes dilatados.
     """
+
     def __init__(self):
         pass
 
-    def removeBlackBackground(self, gray_crop):
+    def remove_black_background(self, gray_crop):
         """
-        Eliminación de las franjas superiror e inferior de la imagen.
+        Eliminacion de las franjas superiror e inferior de la imagen.
 
         :param gray_crop: imagen cortada
         :return: imagen cortada
         """
-        thresholdBlack = 90
-        
-        meanColor = np.mean(gray_crop)
+        threshold_black = 90
+
+        mean_color = np.mean(gray_crop)
         for i in range(gray_crop.shape[1]):
             for j in range(gray_crop.shape[0]):
-                if(gray_crop[j][i] < thresholdBlack):
-                    gray_crop[j][i] = meanColor
+                if gray_crop[j][i] < threshold_black:
+                    gray_crop[j][i] = mean_color
                 else:
-                    gray_crop[j][i] = meanColor
+                    gray_crop[j][i] = mean_color
                     break
 
         for i in range(gray_crop.shape[1]):
             for j in range(gray_crop.shape[0]):
                 row_index = gray_crop.shape[0] - j - 1
                 col_index = gray_crop.shape[1] - i - 1
-                if(gray_crop[row_index][col_index] < thresholdBlack):
-                    gray_crop[row_index][col_index] = meanColor
+                if gray_crop[row_index][col_index] < threshold_black:
+                    gray_crop[row_index][col_index] = mean_color
                 else:
-                    gray_crop[row_index][col_index] = meanColor
+                    gray_crop[row_index][col_index] = mean_color
                     break
 
         return gray_crop
 
     def canny_filter(self, img):
         """
-        Filtrado de canny para búsqueda de bordes
+        Filtrado de canny para busqueda de bordes
 
         :param img: imagen
         :return: imagen de bordes
         """
+        th_mean = 0.5
+        th_1 = 50
+        th_2 = 100
         mean = np.mean(img)
-        img[img > mean*0.5] = 0
-        return cv2.Canny(img, 50, 100)
+        img[img > mean * th_mean] = 0
+        return cv2.Canny(img, th_1, th_2)
 
     def crop_image(self, img, contours):
         """
-        Recorte de la imagen
+        Recorte de la imagen en funcion de los valors
+        obtenidos anteriormente en la funcion get_contours
 
         :param img: imagen
         :param contours: contornos
         :return: imagen cortada
         """
-        
-        percentageToCrop = 0.15
-        
+
+        percentage_to_crop = 0.15
+
         cnt = contours[0]
         x, y, w, h = cv2.boundingRect(cnt)
-        yCrop = y+int(h*percentageToCrop)
-        xCrop = x+int(w*percentageToCrop)
+        y_crop = y + int(h * percentage_to_crop)
+        x_crop = x + int(w * percentage_to_crop)
         return img[
-                    y+int(h*percentageToCrop):y+h-int(h*percentageToCrop),
-                    x+int(w*percentageToCrop):x+w-int(w*percentageToCrop)
-                    ],xCrop, yCrop
+               y + int(h * percentage_to_crop):y + h - int(h * percentage_to_crop),
+               x + int(w * percentage_to_crop):x + w - int(w * percentage_to_crop)
+               ], x_crop, y_crop
 
     def get_contours(self, img):
         """
-        Obtención de contornos
+        Obtencion de contornos
 
         :param img: imagen
         :return: lista de contornos
         """
-        _, thresh = cv2.threshold(img, 100, 150, cv2.THRESH_BINARY)
+        threshold = 100
+        max_value = 150
+        _, thresh = cv2.threshold(img, threshold, max_value, cv2.THRESH_BINARY)
         return cv2.findContours(
-                                thresh,
-                                cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
+            thresh,
+            cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
 
     def convert_image_to_gray(self, img):
         """
-        Método para convertir la imagen a escala de grises
+        Metodo para convertir la imagen a escala de grises
 
         :param img: imagen de entrada en BGR
         :param th: threshold
@@ -98,10 +105,12 @@ class Preprocess():
 
     def morphology(self, type_m, img_gray, kernel_size=3, iterations=1):
         """
-        Operación morfológica de erosión
+        Operacion morfologica de erosion
 
+        :param type_m: Tipo de operacion a realizar
         :param img_gray: imagen en escala de grises
-        :param kernel_size: tamaño del elemento estructurante
+        :param kernel_size: tamano del elemento estructurante
+        :param iterations: Numero de iteracciones a realizar
         :return: imagen erosionada/dilatada
         """
         kernel = np.ones((kernel_size, kernel_size), np.uint8)
