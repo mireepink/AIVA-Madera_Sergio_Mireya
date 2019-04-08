54b727ca7ddf7a03782e0d94bab7a3e7be774014
Sergio <sergiodurjc@gmail.com>
2019-04-08 17:41:29 +0200

    Modificados test

--- test_unit/detection/test_detection.py
+++ test_unit/detection/test_detection.py
@@ -10,9 +10,9 @@ class TestDetection(TestCase):
     def setUp(self):
         self.path_out = os.path.join(os.pardir, 'AIVA-Madera_Sergio_Mireya/test_unit/data')
         self.path_im1 = os.path.join(os.pardir, 'AIVA-Madera_Sergio_Mireya/test_unit/data/st1526')
-        self.path_im2 = os.path.join(os.pardir, 'AIVA-Madera_Sergio_Mireya/test_unit/data/st1456')
+        self.path_im2 = os.path.join(os.pardir, 'AIVA-Madera_Sergio_Mireya/test_unit/data/st1012')
         self.path_im1_out = os.path.join(os.pardir, 'AIVA-Madera_Sergio_Mireya/test_unit/data/st1526_split.jpg')
-        self.path_im2_out = os.path.join(os.pardir, 'AIVA-Madera_Sergio_Mireya/test_unit/data/st1456_split.jpg')
+        self.path_im2_out = os.path.join(os.pardir, 'AIVA-Madera_Sergio_Mireya/test_unit/data/st1012_split.jpg')
         self.shape_out_expected = (512, 488, 3)
         self.detection = Detection(self.path_out)
 
